9944a68781b399b4f51f566a17793b3662f527de
Anushka Weerasekarage <w.anushka@gmail.com>
2018-07-11 10:24:32 -0400

    Single Page Application

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,11 +1,13 @@
 import { Comment } from './comment';
 
 export class Dish {
+    id: number;
     name: String;
     image: String;
     category: String;
     label: String;
     price: String;
+    featured: boolean;
     description: String;
     comments: Comment[];
 }
